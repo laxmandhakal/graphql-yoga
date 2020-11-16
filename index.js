a647c0a478c67bec9d0ac709012247cd30e4ca5b
laxman dhakal <dhakallaxman23@gmail.com>
2020-11-16 10:08:09 +0545

    final commit

--- index.js
+++ index.js
@@ -5,15 +5,7 @@ const typeDefs = require("./graphql/schema/index");
 const resolvers = require("./graphQL/resolver/index");
 const { GraphQLServer } = require("graphql-yoga");
 const app = express();
-// file upload
-// import { createWriteStream } from 'fs'
-// import * as mkdirp from 'mkdirp'
-// import * as shortid from 'shortid'
-// import lowdb = require('lowdb')
-// import FileSync = require('lowdb/adapters/FileSync')
-// const uploadDir = './uploads'
-// const db = new lowdb(new FileSync('db.json'))
-//
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
